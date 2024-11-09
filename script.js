a1ec5c319dc7708df067e85419b0fc8b007f3d26
Fikiuq <100091408+Fikiuq@users.noreply.github.com>
2024-11-09 16:14:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function showRules(evt, server) {
+function showRules(evt, serverName) {
     var i, tabcontent, tablinks;
 
-    // Verberg alle tabcontent secties
+    // Verberg alle tab-content elementen
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Verwijder de actieve klasse van alle tablinks
+    // Verwijder de "active" class van alle knoppen
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Toon de juiste tab en voeg de actieve klasse toe
-    document.getElementById(server).style.display = "block";
+    // Toon de geselecteerde tab en voeg de "active" class toe aan de knop
+    document.getElementById(serverName).style.display = "block";
     evt.currentTarget.className += " active";
 }
